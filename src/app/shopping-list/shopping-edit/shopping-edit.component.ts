64cef02b5a58e5d41c2469d77dc4f6b0a0bb275e
Abhijeet-Anand-01 <abhijeetad500@gmail.com>
2023-02-09 14:00:03 +0530

    indentation and filteration

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -18,7 +18,6 @@ export class ShoppingEditComponent implements OnInit {
   }
 
   onAddItem() {
-    console.log(this.nameInputRef);
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
